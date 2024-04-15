c890e18998ec7e5b79001bf740ca8e4347b8e790
wilbergerHannah <166016835+wilbergerHannah@users.noreply.github.com>
2024-04-15 07:31:53 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+let numero1= 242
+let numero2= 2
+let suma= numero1 + numero2
+let resta= numero1 - numero2
+let multiplicacion= numero1 * numero2
+letdivision= numero1 / numero2
+
+
+
+
+console.log ('La suma es:' + (suma))
+console.log ('La resta es:'+ (resta))
+console.log ('La multiplicacion es:' + (multiplicacion))
+console.log ('La division es:' + (division))
+
+
+alert ('La suma es:' + (suma))
+alert ('La resta es:' + (resta))
+alert ('La multiplicacion es:' + (multiplicacion))
+alert ( 'La division es:' + (division))
+
+
+let añoNacimiento= 2009
+let añoActual= 2024
+alert('La edad del alumno es:' +  (2024 - 2009))
+
+
+
+let añonacimiento=  prompt ('Ingrese su año de nacimiento')
+let añoactual= 2024
+let edadAlumno= añoactual - añonacimiento
+alert('La edad del alumno es:' + edadAlumno )
+
+
+
+let nombreAlumno= prompt ( ' Ingrese su nombre ')
+alert( ' Te damos la bienvenida' + (nombreAlumno))
+
+
+
+
+let alumnoNombre= 'Hannah'
+let apellidoAlumno = 'Wilberger'
+console.log('Bienvenida ' + nombreAlumno + apellidoAlumno)
+alert('Bienvenida ' + nombreAlumno + apellidoAlumno)
+
+
+
+
+let diez= 10
+let tres= 3
+let resultadoMultiplicacion= ( diez * tres)
+console.log('El resultado de la multiplicacion es:' + ( resultadoMultiplicacion))
+
